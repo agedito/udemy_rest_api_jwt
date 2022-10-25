cab50dc520c1a9ea95532832724a36736e70e95b
agedito <agedito@gmail.com>
2022-10-25 19:02:18 +0200

    basic endpoints

--- src/main.go
+++ src/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/signup", signup).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+
+	log.Println("Listen on port 8000...")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("SignUp invoked")
+}
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Login invoked")
 }
