96b2c3bd7f69de7116940806788b3725a601616e
agedito <agedito@gmail.com>
2022-10-25 19:36:09 +0200

    endpoint protected by middleware

--- src/main.go
+++ src/main.go
@@ -11,6 +11,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/signup", signup).Methods("POST")
 	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/protected", TokenVerifyMiddleWare(ProtectedEndPoint)).Methods("POST")
 
 	log.Println("Listen on port 8000...")
 	log.Fatal(http.ListenAndServe(":8000", router))
@@ -19,6 +20,19 @@ func main() {
 func signup(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("SignUp invoked")
 }
+
 func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Login invoked")
+}
+
+func ProtectedEndPoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Protected endpoint invoked")
+}
+
+func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
+	callback := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Token Verified!!")
+		next.ServeHTTP(w, r)
+	}
+	return callback
 }
