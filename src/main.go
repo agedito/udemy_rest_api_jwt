ea443b8e8afad2079a47098cdf8f7588abe59eed
agedito <agedito@gmail.com>
2022-10-29 21:48:36 +0200

    middleware

--- src/main.go
+++ src/main.go
@@ -11,6 +11,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type User struct {
@@ -151,11 +152,41 @@ func ProtectedEndPoint(w http.ResponseWriter, r *http.Request) {
 }
 
 func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
-	callback := func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Token Verified!!")
-		next.ServeHTTP(w, r)
-	}
-	return callback
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var errorObject Error
+		authHeader := r.Header.Get("Authorization")
+		bearerToken := strings.Split(authHeader, " ")
+
+		if len(bearerToken) == 2 {
+			authToken := bearerToken[1]
+
+			token, error := jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error")
+				}
+
+				return []byte("secret"), nil
+			})
+
+			if error != nil {
+				errorObject.Message = error.Error()
+				respondWithError(w, http.StatusUnauthorized, errorObject)
+				return
+			}
+
+			if token.Valid {
+				next.ServeHTTP(w, r)
+			} else {
+				errorObject.Message = error.Error()
+				respondWithError(w, http.StatusUnauthorized, errorObject)
+				return
+			}
+		} else {
+			errorObject.Message = "Invalid token."
+			respondWithError(w, http.StatusUnauthorized, errorObject)
+			return
+		}
+	})
 }
 
 func respondWithError(w http.ResponseWriter, status int, error Error) {
