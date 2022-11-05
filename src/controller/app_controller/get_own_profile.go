bd14b0de0bad9abbb8126a418721d725d923922f
agedito <agedito@gmail.com>
2022-11-05 19:29:24 +0100

    get own profile

--- src/controller/app_controller/get_own_profile.go
+++ src/controller/app_controller/get_own_profile.go
@@ -1,13 +1,30 @@
 package app_controller
 
 import (
-	"fmt"
+	"agedito/udemy/rest_api_jwt/utils"
+	"errors"
 	"net/http"
 )
 
-// FEATURE Implement this
+var InvalidEmailError = errors.New("invalid error")
+var NotFindUserError = errors.New("no find user")
+
 func (c *AppController) GetOwnProfile(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Protected")
 	email, err := c.getEmailFromTokenRequest(w, r)
-	fmt.Println(email, err)
+	if utils.AssertError(err) {
+		c.responseError(w, http.StatusUnauthorized, InvalidEmailError)
+	}
+
+	user, exists, repoErr := c.Repo.FindUser(email)
+	if !exists {
+		c.responseError(w, http.StatusUnauthorized, repoErr)
+	}
+
+	if !exists {
+		c.responseError(w, http.StatusUnauthorized, NotFindUserError)
+	}
+
+	data := make(map[string]string)
+	data["email"] = user.Email
+	c.responseJson(w, http.StatusOK, data)
 }
